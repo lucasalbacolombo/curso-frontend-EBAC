8eb37b76563dd64ea7979b408d6dd7845235a6dd
Lucas Colombo <colombo.lucas@outlook.com>
2022-03-26 16:13:34 -0300

    bootstrap-project

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,71 +1,18 @@
-function calcularMedia( notas ) {
+function calcularPrestacoes(numero_prestacao) {
 
-    let soma = 0;
-    for( c = 0; c < notas.length; c++) {
-        soma += notas[c];
-    }
+    let valor = {};
+    let prestacao = {};
 
-    media = soma / notas.length;
+    numero_prestacao = (valor - 3000) / prestacao
 
-    return media;
-
+    return numero_prestacao;
 }
 
-let media; // escopo global
+    var form = document.getElementById('formulario-01');
 
-function aprovacao( notas ) {
-
-    let media = calcularMedia( notas ); // escopo da função
-
-    let condicao = media >= 8 ? "aprovado" : "reprovado";
-
-    return 'Média: ' + media + ' - Resultado: ' + condicao;
-
-}
-
-
-// Função Recursivas
-
-function contagemRegressiva(numero){
-
-    console.log(numero);  
-    
-    let proximoNumero = numero - 1;
-
-    if(proximoNumero > 0)
-        contagemRegressiva(proximoNumero);
-
-}
-
-// contagemRegressiva(50);
-
-document.addEventListener('submit', function( evento ){
-
-    evento.preventDefault();
-    evento.stopPropagation();
-
-    let formulario = document.getElementById('formulario-01');
-
-    let dados = new FormData(formulario);
-
-    let objeto = {};
-
-    let notas = [];
-
-    for(let key of dados.keys()) {
-        objeto[key] = dados.get(key);
-
-        // adicionar itens no array
-        notas.push( parseInt(dados.get(key)));
-
-    }
-
-    console.log(notas);
-
-    console.log(objeto);
-
-    texto = aprovacao(notas)
+    form.addEventListener(function(event) {
+    event.preventDefault()
+    var dados = new FormData(this);
+})
 
     document.getElementById('resultado').innerHTML = texto;
-
-});
